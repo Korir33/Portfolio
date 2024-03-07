f682be10a8bbb15e2beecfdc800ce76a870bd328
Korir <mathewkorir1@gmail.com>
2024-03-07 13:35:20 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+print("Hello?")
+name = input('What is your name? ')
+age = int ( input('How old are you? '))
+location = input('Give us your location? ')
+
+print (f'Hello {name}! and welcome to Power Learn Project, you are {age} years old and live in {location}')
